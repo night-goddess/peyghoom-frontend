496895fcfc2564780f1f42513568261aff576659
reza-ya <rezaayaari@gmail.com>
2025-08-01 19:47:59 +0330

    add rezayari.ir address to api.ts

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,5 +1,7 @@
 export const urls = {
     // baseUrl: "https://localhost:5000"
     // baseUrl: " https://localhost:7285"
-    baseUrl: "https://localhost:7285"
+    // baseUrl: "https://localhost:7285"
+    // baseUrl: "http://localhost:91"
+    baseUrl: "https://rezayari.ir:90"
 }
